158b1f5949c7280e99b2678c0619a393c9c41ba3
Brayan Montestruqu <brayan@MacBook-Air-de-Brayan.local>
2025-10-14 00:58:51 -0300

    Codigo final con todo incluido y cosas eliminadas

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ import Scroll from './utils/scroll.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-  <BrowserRouter basename={import.meta.env.BASE_URL}>
+  <BrowserRouter>
     <Scroll />
     <App />
   </BrowserRouter>
--- vite.config.js
+++ vite.config.js
@@ -13,10 +13,5 @@ export default defineConfig({
       },
     }),
   ],
-    base: isGitHubPages ? '/proyecto-cibercuritas/' : './',
-
-    build: {
-    outDir: 'docs',
-  },
-
+    base: './',
 })
